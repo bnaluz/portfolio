8ed267b85cc9a4f54a87beaa7ca54f985cb904e2
Bobby Naluz <bnaluz9@gmail.com>
2023-01-26 19:44:50 -0500

    added in routing for legilogin and port

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import PortfolioImg from '/Portfolio.jpg'
+import PortfolioImg from './Portfolio.jpg'
 import {RiRadioButtonFill} from 'react-icons/ri'
 import Link from "next/link";
 
