983d838bd5c32636a98dfde81bebb3e5a1a69a1a
Bobby Naluz <bnaluz9@gmail.com>
2023-03-02 12:11:02 -0800

    fix:sbPage img

--- src/pages/sbdupe.tsx
+++ src/pages/sbdupe.tsx
@@ -6,7 +6,7 @@ const sbdupe = () => {
     return <div className='w-full'>
     <div className='w-screen h-[50vh] relative'>
       <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
-                <Image  className='absolute z-1' layout='fill' objectFit='cover' src={'/SbDupePage.png'} alt='/' />
+                <Image  className='absolute z-1' layout='fill' objectFit='cover' src={'/sbPage.png'} alt='/' />
                 <div className="absolute top-[70%] max-w-[1440px] w-full left-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
                 <a href='https://sb-dupe.vercel.app/'
                         target='_blank'
