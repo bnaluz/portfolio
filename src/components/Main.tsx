d5d37d52ffc39a478c66187d9dbb525172e7d1fa
Bobby Naluz <bnaluz9@gmail.com>
2024-10-17 21:12:44 -0400

    update: main text

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,13 +8,13 @@ const Main = () =>{
         <div id='main' className='w-full h-screen text-center'>
             <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
                 <div >
-                    <p className='uppercase text-sm tracking-widest text-gray-600 pt-5'>Let's build something amazing together.</p>
+                    <p className='uppercase text-sm tracking-widest text-gray-600 pt-5'>Let's build something impactful together.</p>
                     <h1 className='py-4 text-gray-700'>
                         Hi, I'm <span className='text-blue-600'>Bobby</span>
                     </h1>
-                    <h1 className='py-4 text-gray-700'>A Front-End Web Developer</h1>
+                    <h1 className='py-4 text-gray-700'>A Full-Stack Developer</h1>
                     <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
-                    As a web developer, I am passionate about leveraging modern technologies to create exceptional digital experiences for users. With a focus on functionality and aesthetics, I strive to deliver projects that exceed client expectations. If you are interested in working with a skilled and dedicated developer, I would be delighted to connect with you and discuss your needs. Let's build something great together!
+                    I&rsquo;m a developer with a passion for building responsive, user-friendly web applications that make a real difference. With a focus on clean design, efficient code, and seamless user experiences, I strive to create solutions that not only look great but function flawlessly. Whether you&rsquo;re looking to develop a new product from scratch or improve an existing one, I&rsquo;d love to bring your vision to life. Let&rsquo;s connect and build something meaningful together!
                     </p>
                     <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
                     <a href="https://www.linkedin.com/in/bobby-naluz-664283197"
